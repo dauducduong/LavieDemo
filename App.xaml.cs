88b2e9b6404cf526b4287864a4d487b728bf33d8
dauducduong <ducduong7a@gmail.com>
2025-01-16 11:51:22 +0700

    Add Login Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows;
+
+namespace LavieDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        [STAThread]
+        public static void Main()
+        {
+            var app = new App();
+            var mainWindow = new MainWindow();
+            app.Run(mainWindow);
+        }
+    }
+}
---
+++ LoginWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LavieDemo
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public bool IsAuthenticated { get; private set; }
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_login_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameTextBox.Text;
+            string password = PasswordBox.Password;
+
+            if (username == "admin" && password == "admin")
+            {
+                IsAuthenticated = true;
+                MessageBox.Show("Đăng nhập thành công!", "Thông báo", MessageBoxButton.OK, MessageBoxImage.Information);
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Tài khoản hoặc mật khẩu không đúng.", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
